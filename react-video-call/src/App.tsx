38b6d1c1da51ae96f31ab63ba9882bf3bd401e4b
Daniel Lim <DanielLimWeiEn@gmail.com>
2024-06-23 00:31:56 +0800

    added basic video conferencing functionality

--- react-video-call/src/App.tsx
+++ react-video-call/src/App.tsx
@@ -1,39 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './index.css'
+import { CallingState, StreamCall, StreamVideo, StreamVideoClient, useCallStateHooks, User, StreamTheme, StreamVideoParticipant, ParticipantView } from '@stream-io/video-react-sdk';
 
+import '@stream-io/video-react-sdk/dist/css/styles.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const apiKey = 'mmhfdzb5evj2';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiSmFjZW5fU29sbyIsImlzcyI6Imh0dHBzOi8vcHJvbnRvLmdldHN0cmVhbS5pbyIsInN1YiI6InVzZXIvSmFjZW5fU29sbyIsImlhdCI6MTcxOTA2NzQ0MCwiZXhwIjoxNzE5NjcyMjQ1fQ.wWGGXsJqq0P9IsqLGi4kPjF72rtKo36-vgHE_YeziOM';
+const userId = 'Jacen_Solo';
+const callId = 'H5aCe5WV0lox';
 
+// set up the user object
+const user: User = {
+  id: userId,
+  name: 'Oliver',
+  image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
+};
+
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call('default', callId);
+await call.join({ create: true });
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <MyUILayout />
+      </StreamCall>
+    </StreamVideo>
+  );
+}
+
+export const MyParticipantList = (props: { participants: StreamVideoParticipant[] }) => {
+  const { participants } = props;
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row', gap: '8px' }}>
+      {participants.map((participant) => (
+        <ParticipantView participant={participant} key={participant.sessionId} />
+      ))}
+    </div>
+  );
+};
+
+export const MyFloatingLocalParticipant = (props: { participant?: StreamVideoParticipant }) => {
+  const { participant } = props;
+  return (
+    <div
+      style={{
+        position:"absolute",
+        top:"15px",
+        left:"15px",
+        width:"240px",
+        height:"135px",
+        boxShadow:"rgba(0,0,0, 0.1) 0px 0px 10px 3px",
+        borderRadius:"12px"
+      }}
+    >
+      <ParticipantView participant={participant!} />
+    </div>
   )
 }
 
-export default App
+export const MyUILayout = () => {
+  const { useCallCallingState, useLocalParticipant, useRemoteParticipants } = useCallStateHooks();
+
+  const callingState = useCallCallingState();
+  const localParticipant = useLocalParticipant();
+  const remoteParticipants = useRemoteParticipants();
+
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <StreamTheme>
+      <MyParticipantList participants={remoteParticipants} />
+      <MyFloatingLocalParticipant participant={localParticipant} />
+    </StreamTheme>
+  );
+};
