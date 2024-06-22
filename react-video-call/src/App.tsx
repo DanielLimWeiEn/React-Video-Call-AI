c79dc1b32353d426d8e8c7bfe6beb50877bcc40b
J1ng00 <wongjl@hotmail.com>
2024-06-23 00:06:29 +0800

    Installed Tailwind

--- react-video-call/src/App.tsx
+++ react-video-call/src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
