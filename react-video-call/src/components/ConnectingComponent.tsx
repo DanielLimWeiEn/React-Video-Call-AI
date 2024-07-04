af04021bd4f4a1ae18fcd0cfaf32867bbb532924
Daniel Lim <DanielLimWeiEn@gmail.com>
2024-07-04 19:59:34 +0800

    add code to update enable websocket connection

--- react-video-call/src/components/ConnectingComponent.tsx
+++ react-video-call/src/components/ConnectingComponent.tsx
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client'
 
 const socket = io("http://localhost:8765");
 export const ConnectingComponent = () => {
-    const [ content, setContent ] = useState({});
+    const [ content, setContent ] = useState({ message: "", timestamp: "" });
     
     useEffect(() => {
         socket.on('connect', () => {
@@ -20,10 +20,10 @@ export const ConnectingComponent = () => {
     }, []);
 
   return (
-    <div style={{display: "flex", justifyContent: "flex-end"}}>
-        <h2>Websocket Client</h2>
-        <h2>{content.timestamp}</h2>
-        <h2>{content.message}</h2>
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center", fontSize: 50, height: "100vh", flexDirection: "column"}}>
+        <h2 style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100%"}}>Websocket Client</h2>
+        <h2 style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100%"}}>{content.timestamp}</h2>
+        <h2 style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100%"}}>{content.message}</h2>
     </div>
   )
 }
